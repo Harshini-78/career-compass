6473c799fc71c1e0e4075f271a32a8357a834a5d
Harshini <satya.singam777@gmail.com>
2026-05-12 11:57:55 +0530

    Setup Django backend deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-sn$qvd3bkow1=84$)cv&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
